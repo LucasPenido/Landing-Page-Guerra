443731fd43786e7b161f42be0ffbb64babe36050
LucasPenido <lpenidoa@me.com>
2020-08-02 16:55:00 -0300

    Adicionando Componentes à página.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Row } from 'react-bootstrap';
+import PhotosSlide from './Components/PhotoSlide/PhotosSlide';
+import Forms from './Components/Forms/Forms';
+import CardsInfo from './Components/CadsInfo/CardsInfo';
+import Header from './Components/Header/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<Header />
+			<div className='container'>
+				<Row className='Row'>
+					<div className='InfoView col-md-6 col-xl-7 rounded'>
+						<h1>Querendo comprar carros de luxo?</h1>
+						<PhotosSlide />
+						<CardsInfo />
+					</div>
+					<div className='InfoForm col-md-6 col-xl-5 d-flex'>
+						<Forms />
+					</div>
+				</Row>
+			</div>
+		</div>
+	);
+};
 
 export default App;
